dee8252712d11ee243b30422d9652a25a3a87f88
Ian Ramirez España <ianespana@hotmail.com>
2021-05-14 10:18:06 -0500

    Cambiar nombre y agregar codigo

--- Proyecto Final/Java/Ejercicio 1/src/com/ianespana/Main.java
+++ Proyecto Final/Java/Ejercicio 1/src/com/ianespana/Main.java
@@ -1,8 +1,19 @@
 package com.ianespana;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import static java.lang.Integer.parseInt;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Dame el valor de la base:");
+        Integer width = parseInt(reader.readLine());
 
-    public static void main(String[] args) {
-	// write your code here
+        System.out.println("Dame el valor de la altura:");
+        Integer height = parseInt(reader.readLine());
+
+        System.out.println("El area es: " + (width*height)/2);
     }
 }
