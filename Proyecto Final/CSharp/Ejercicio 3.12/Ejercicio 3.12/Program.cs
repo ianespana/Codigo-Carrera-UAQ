dcf4c716b3b4ed77cdd14fc80db55a4bd568262f
Ian Ramirez-España <ianespana@users.noreply.github.com>
2021-05-15 21:31:29 -0500

    Undo Temp

---
+++ Proyecto Final/CSharp/Ejercicio 3.12/Ejercicio 3.12/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Ejercicio_3._12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double total = 0;
+            Console.WriteLine("Dame el total de horas trabajadas:");
+            float horas = float.Parse(Console.ReadLine());
+
+            if (horas > 50)
+            {
+                Console.WriteLine("Trabajar más de 50 horas no está permitido");
+                return;
+            }
+
+            Console.WriteLine("Dame el sueldo por hora:");
+            float sueldo = float.Parse(Console.ReadLine());
+
+            for (int x = 1; x <= horas; x++)
+            {
+                total += CalculatePrice(x, sueldo);
+            }
+
+            Console.WriteLine("El pago semanal es de ${0}", total);
+        }
+
+        static double CalculatePrice(int horas, float sueldo)
+        {
+            if (horas > 0 && horas <= 40)
+            {
+                return sueldo;
+            }
+            else if (horas > 40 && horas <= 45)
+            {
+                return sueldo*2;
+            }
+            else if (horas > 45 && horas <= 50)
+            {
+                return sueldo*3;
+            }
+            else
+            {
+                Console.WriteLine("El número de horas no es válido");
+                return 0;
+            }
+        }
+    }
+}
---
+++ Proyecto Final/CSharp/Ejercicio 3.14/Ejercicio 3.14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ejercicio_3._14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dame una calificación (0-10):");
+            int grade = int.Parse(Console.ReadLine());
+
+            switch (grade)
+            {
+                case 10:
+                    Console.WriteLine("La calificación equivalente es: A");
+                    break;
+                case 9:
+                    Console.WriteLine("La calificación equivalente es: B");
+                    break;
+                case 8:
+                    Console.WriteLine("La calificación equivalente es: C");
+                    break;
+                case 7:
+                    Console.WriteLine("La calificación equivalente es: D");
+                    break;
+                case 6:
+                    Console.WriteLine("La calificación equivalente es: D");
+                    break;
+                default:
+                    Console.WriteLine("La calificación equivalente es: F");
+                    break;
+            }
+        }
+    }
+}
