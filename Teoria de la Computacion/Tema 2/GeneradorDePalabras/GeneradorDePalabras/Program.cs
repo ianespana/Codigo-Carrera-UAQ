03f8d046fac9fd69f96f0547998fe016f24ac87a
Ian Ramirez-España <ianespana@users.noreply.github.com>
2022-08-23 14:53:45 -0500

    Generador de palabras

---
+++ Teoria de la Computacion/Tema 2/GeneradorDePalabras/GeneradorDePalabras/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace GeneradorDePalabras
+{
+    internal class Program
+    {
+        static List<string> alphabet = new();
+        static int wordSize = 0;
+
+        static void Concat(string oldString) {
+            if (oldString.Length > wordSize)
+            {
+                return;
+            }
+
+            string word = oldString;
+            Console.WriteLine(word);
+            foreach (string letter in alphabet)
+            {
+                Concat(word + letter);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dame el numero de letras que hay en el alfabeto:");
+            int alphabetSize = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < alphabetSize; i++) {
+                Console.WriteLine("Dame la letra " + (i + 1));
+                alphabet.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine("Dame la longitud máxima de las palabras:");
+            wordSize = int.Parse(Console.ReadLine());
+            Console.WriteLine("NULL");
+
+            for (int i = 0; i < alphabet.Count; i++) {
+                Concat(alphabet[i]);
+            }
+
+            Console.WriteLine("Quieres repetir el programa? (S/N)");
+            string restart = Console.ReadLine();
+            if (restart.ToLower() == "s") {
+                Console.Clear();
+                Main(null);
+            }
+        }
+    }
+}
