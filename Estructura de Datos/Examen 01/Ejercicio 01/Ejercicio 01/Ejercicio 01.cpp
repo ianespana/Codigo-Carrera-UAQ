6616fe8dc98663af7971fcff4eb50c29387e57bd
Ian Ramirez-España <ianespana@users.noreply.github.com>
2022-09-07 07:55:37 -0500

    Update Ejercicio 01.cpp

--- Estructura de Datos/Examen 01/Ejercicio 01/Ejercicio 01/Ejercicio 01.cpp
+++ Estructura de Datos/Examen 01/Ejercicio 01/Ejercicio 01/Ejercicio 01.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 
+void recursive(bool par, int num, int contador) {
+    if (contador < num) {
+        recursive(!par, num, contador + 1);
+    }
+    else if (par) {
+        std::cout << "\nEs un numero par.";
+    }
+    else {
+        std::cout << "\nEs un numero impar.";
+    }
+}
+
 int main()
 {
     int num;
     std::cout << "Dame un numero: ";
     std::cin >> num;
-
-    if (num % 2 == 0) {
-        std::cout << "\nEs un numero par.";
-    } else {
-        std::cout << "\nEs un numero impar.";
-    }
+    recursive(true, num, 0);
 }
