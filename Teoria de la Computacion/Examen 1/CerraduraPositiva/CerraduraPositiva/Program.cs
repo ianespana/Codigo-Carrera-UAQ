d8ef020949b57d9f40d4fc59d58703d988c4bc93
Ian Ramirez-España <ianespana@users.noreply.github.com>
2022-09-20 13:55:45 -0500

    Examen 1 - Teoria de la Computacion

---
+++ Teoria de la Computacion/Examen 1/CerraduraPositiva/CerraduraPositiva/Program.cs
@@ -0,0 +1,75 @@
+﻿namespace CerraduraPositiva
+{
+    internal class Program
+    {
+        static string alphabet = "";
+        static int wordSize = 0;
+        static List<string> language = new();
+
+        static void Concat(string oldString)
+        {
+            Console.WriteLine("\t" + oldString);
+            language.Add(oldString);
+            if (oldString.Length >= wordSize)
+            {
+                return;
+            }
+
+            foreach (char letter in alphabet)
+            {
+                Concat(oldString + letter);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            alphabet = "";
+            wordSize = 0;
+
+            Console.WriteLine("Dame el alfabeto:");
+            alphabet = Console.ReadLine();
+
+            Console.WriteLine("Dame la longitud máxima de las palabras:");
+            wordSize = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\n\nLenguaje con palabras de longitud 0 a " + alphabet.Length + ":\n");
+            Console.WriteLine("\tNULL");
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                Concat(alphabet[i].ToString());
+            }
+
+            Console.WriteLine("\n\nPalabras del lenguaje con una cantidad par de símbolos:\n");
+            Console.WriteLine("\tNULL");
+
+            foreach (string word in language)
+            {
+                Dictionary<char, int> simbolCount = new();
+                foreach (char letter in word)
+                {
+                    if (simbolCount.ContainsKey(letter))
+                    {
+                        simbolCount[letter]++;
+                    }
+                    else
+                    {
+                        simbolCount[letter] = 1;
+                    }
+                }
+
+                bool even = true;
+                foreach(KeyValuePair<char, int> pair in simbolCount)
+                {
+                    if (pair.Value % 2 == 1)
+                    {
+                        even = false;
+                        break;
+                    }
+                }
+
+                if (even) Console.WriteLine("\t" + word);
+            }
+        }
+    }
+}
---
+++ Teoria de la Computacion/Examen 1/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Palindrome
+{
+    internal class Program
+    {
+        static string Reverse(string text)
+        {
+            if (text == null) return null;
+
+            char[] array = text.ToCharArray();
+            Array.Reverse(array);
+            return new string(array);
+        }
+
+        static bool IsPalindome(string word)
+        {
+            string reversedWord = Reverse(word);
+            return word == reversedWord;
+        }
+
+        static bool IsMadeOfAlphabet(string word, string alphabet)
+        {
+            foreach (char wordLetter in word)
+            {
+                bool match = false;
+                foreach(char alphabetLetter in alphabet)
+                {
+                    if (wordLetter == alphabetLetter)
+                    {
+                        match = true;
+                        break;
+                    }
+                }
+                
+                if (!match) return false;
+            }
+
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa el alphabeto Σ:");
+            string alphabet = Console.ReadLine();
+
+            Console.WriteLine("Ingresa una palabra:");
+            string word = Console.ReadLine();
+
+            if (IsPalindome(word))
+            {
+                Console.WriteLine("La palabra es palindrome");
+            }
+            else
+            {
+                Console.WriteLine("La palabra no es palindrome");
+            }
+
+            if (IsMadeOfAlphabet(word, alphabet))
+            {
+                Console.WriteLine("La palabra es parte de Σ*");
+            }
+            else
+            {
+                Console.WriteLine("La palabra no es parte de Σ*");
+            }
+        }
+    }
+}
