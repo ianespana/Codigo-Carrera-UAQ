479ea7a3d93ee5c41a020818a4b67bd3d8c1cff2
Ian Ramirez-España <ianespana@users.noreply.github.com>
2022-09-21 08:24:26 -0500

    Update Actividad 08.cpp

--- Estructura de Datos/Actividad 08/Actividad 08/Actividad 08.cpp
+++ Estructura de Datos/Actividad 08/Actividad 08/Actividad 08.cpp
@@ -5,7 +5,14 @@ struct Nodo {
     Nodo* siguiente;
 };
 
-void insertarLista(Nodo* &lista, int n) {
+Nodo* lista;
+
+void clearConsole() {
+    // CSI[2J clears screen, CSI[H moves the cursor to top-left corner
+    std::cout << "\x1B[2J\x1B[H";
+}
+
+void insertarElemento(Nodo* &lista, int n) {
     Nodo* nuevo_nodo = new Nodo();
     nuevo_nodo->dato = n;
 
@@ -27,6 +34,76 @@ void insertarLista(Nodo* &lista, int n) {
     nuevo_nodo->siguiente = aux1;
 }
 
+void mostrarLista(Nodo* lista) {
+    Nodo* actual = lista;
+    while (actual != NULL) {
+        std::cout << actual->dato << " -> ";
+        actual = actual->siguiente;
+    }
+}
+
+void buscarElemento(Nodo* lista, int n) {
+    bool found = false;
+
+    Nodo* actual = lista;
+
+    while (actual != NULL && actual->dato <= n) {
+        if (actual->dato == n) {
+            found = true;
+        }
+
+        actual = actual->siguiente;
+    }
+
+    if (found) {
+        std::cout << "Elemento " << n << " SI ha sido encontrado en lista\n";
+    }
+    else
+    {
+        std::cout << "Elemento " << n << " NO ha sido encontrado en lista\n";
+    }
+}
+
+int menu() {
+    clearConsole();
+    int opcion;
+    int element;
+    std::cout << "Elige una opcion\n";
+    std::cout << "\t[1] Insertar elemento\n";
+    std::cout << "\t[2] Mostrar lista\n";
+    std::cout << "\t[3] Buscar elemento\n";
+    std::cin >> opcion;
+
+    switch (opcion)
+    {
+        case 1:
+            std::cout << "Dame el elemento a insertar\n";
+            std::cin >> element;
+            insertarElemento(lista, element);
+            break;
+        case 2:
+            mostrarLista(lista);
+            std::cout << "\n\n";
+            system("pause");
+            break;
+        case 3:
+            std::cout << "Dame el elemento a buscar\n";
+            std::cin >> element;
+            buscarElemento(lista, element);
+            system("pause");
+            break;
+        default:
+            return 0;
+    }
+
+    return opcion;
+}
+
 int main()
 {
+    do
+    {
+        int opcion = menu();
+        if (opcion == 0) break;
+    } while (true);
 }
