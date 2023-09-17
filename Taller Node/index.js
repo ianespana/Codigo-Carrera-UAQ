6bff697be961fe57f8153f69ceb589c293e07413
Ian Ramirez-España <ianespana@users.noreply.github.com>
2023-09-17 17:49:23 -0600

    Actividad 6

--- Taller Node/index.js
+++ Taller Node/index.js
@@ -1,34 +1,39 @@
 const express = require('express');
 const app = express();
-const pokedex = require('./pokedex.json');
+const { pokemon } = require('./pokedex.json');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.status(200).send('Bienvenido al servidor');
+    return res.status(200).send('Bienvenido al pokedex');
 });
 
-app.get('/pokemon/all', (req, res) => {
-    res.status(200).send(pokedex);
+app.post('/pokemon', (req, res) => {
+    return res.status(200).send(req.body);
+});
+
+app.get('/pokemon', (req, res) => {
+    return res.status(200).send(pokemon);
 });
 
 app.get('/pokemon/:id([0-9]{1-3})', (req, res) => {
     if (req.params.id > 151) {
-        res.status(404).send('No se encontró el pokemon');
-        return;
+        return res.status(404).send('No se encontró el pokemon');
     }
 
-    res.status(200).send(pokedex[req.params.id - 1]);
+    return res.status(200).send(pokemon[req.params.id - 1]);
 });
 
-app.get('/pokemon/:name', (req, res) => {
+app.get('/pokemon/:name([a-zA-Z]+)', (req, res) => {
     const name = req.params.name.toLowerCase();
-    const pokemon = pokedex.find((p) => p.name.toLowerCase() === name);
+    const pk = pokemon.find((p) => p.name.toLowerCase() === name);
 
-    if (!pokemon) {
-        res.status(404).send('No se encontró el pokemon');
-        return;
+    if (!pk) {
+        return res.status(404).send('No se encontró el pokemon');
     }
 
-    res.status(200).send(pokemon);
+    return res.status(200).send(pk);
 });
 
 app.listen(process.env.PORT || 3000, () => {
