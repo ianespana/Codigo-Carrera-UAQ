a22fe5f05f30bfc27e891cc2c26e9cbe8330f6fb
Ian Ramirez-España <ianespana@users.noreply.github.com>
2023-10-01 23:17:49 -0600

    Añadir SQL y Users

--- Taller Node/index.js
+++ Taller Node/index.js
@@ -1,18 +1,21 @@
 const morgan = require('morgan');
 const express = require('express');
-const app = express();
-const pokemonRouter = require('./routes/pokemon')
+const app = express ();
+const pokemon = require('./routes/pokemon');
+const user = require('./routes/user');
 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    return res.status(200).send('Bienvenido al pokedex');
+app.get("/", (req, res, next) => {
+    return res.status(200).send("Bienvenido al Pokedex");
 });
-
-app.use('/pokemon', pokemonRouter);
-
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+app.use("/pokemon", pokemon);
+app.use("/user", user);
+app.use((req, res, next) => {
+    return res.status(404).send("Ruta no encontrada");
+});
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log("Server is runing...");
 });
--- Taller Node/routes/pokemon.js
+++ Taller Node/routes/pokemon.js
@@ -1,32 +1,79 @@
 const express = require('express');
-const pokemonRouter = express.Router();
-const { pokemones } = require('../pokedex.json');
+const pokemon = express.Router();
+const db = require('../config/database');
 
-pokemonRouter.post('/', (req, res) => {
-	return res.status(200).send(req.body);
+pokemon.post('/', async (req, res, next) => {
+    const {pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
+    if (pok_name && pok_height && pok_weight && pok_base_experience) {
+        let query = "INSERT INTO pokemon(pok_name, pok_height, pok_weight, pok_base_experience)";
+        query += ` VALUES('${pok_name}', ${pok_height}, ${pok_weight}, ${pok_base_experience})`;
+        const rows = await db.query(query);
+
+        if (rows.affectedRows == 1) {
+            return res.status(201).json({code: 201, message: "Pokemon insertado correctamente"});
+        }
+        return res.status(500).json({code: 500, message: "Ocurrió un error"});
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"});
 });
 
-pokemonRouter.get('/', (req, res) => {
-	return res.status(200).send(pokemones);
+pokemon.delete('/:id([0-9]{1,3})', async (req, res, next) => {
+    const query = `DELETE FROM pokemon WHERE pok_id=${req.params.id}`;
+    const rows = await db.query(query);
+    if (rows.affectedRows == 1) {
+        return res.status(200).json({code: 200, message: "Pokemon borrado correctamente"});
+    }
+    return res.status(404).json({code: 404, message: "Pokemon no encontrado"});
+});
+pokemon.put('/:id([0-9]{1,3})', async (req, res, next) => {
+    const {pok_name, pok_height, pok_weight, pok_base_experience} = req.body;
+
+    if (pok_name && pok_height && pok_weight && pok_base_experience) {
+        let query = `UPDATE pokemon SET pok_name='${pok_name}', pok_height=${pok_height},`;
+        query += `pok_weight=${pok_weight}, pok_base_experience=${pok_base_experience} WHERE pok_id=${req.params.id};`;
+
+        const rows = await db.query(query);
+
+        if (rows.affectedRows == 1) {
+            return res.status(200).json({code: 200, message: "Pokemon actualizado correctamente"});
+        }
+        return res.status(500).json({code: 500, message: "Ocurrió un error"});
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"});
 });
 
-pokemonRouter.get('/:id([0-9]{1-3})', (req, res) => {
-	if (req.params.id > 151) {
-		return res.status(404).send('No se encontró el pokemon');
-	}
+pokemon.patch('/:id([0-9]{1,3})', async (req, res, next) => {
+    if (req.body.pok_name) {
+        let query = `UPDATE pokemon SET pok_name='${req.body.pok_name}' WHERE pok_id=${req.params.id};`;
+        const rows = await db.query(query);
 
-	return res.status(200).send(pokemones[req.params.id - 1]);
+        if (rows.affectedRows == 1) {
+            return res.status(200).json({code: 200, message: "Pokemon actualizado correctamente"});
+        }
+        return res.status(500).json({code: 500, message: "Ocurrió un error"});
+    }
+    return res.status(500).json({code: 500, message: "Campos incompletos"});
+});
+pokemon.get('/', async (req, res, next) => {
+    const pkmn = await db.query("SELECT * FROM pokemon");
+    return res.status(200).json({ code: 1, message: pkmn });
+});
+pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
+    const id = req.params.id;
+    if (id >= 1 && id <= 722) {
+        const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_id=" + id + ";");
+        return res.status(200).json({ code: 1, message: pkmn });
+    }
+    return res.status(404).send({ code: 404, message: "Pokemon no encontrado" })
+    });
+
+pokemon.get('/:name([A-Za-z]+)', async (req,res, next) =>{
+    const name = req.params.name;
+    const pkmn = await db.query("SELECT * FROM pokemon WHERE pok_name='" + name + "';");
+    if (pkmn.length > 0){
+        return res.status(200).json({ code: 1, message: pkmn });
+    }
+    return res.status(404).send({ code: 404, message: "Pokemon no encontrado" })
 });
 
-pokemonRouter.get('/:name([a-zA-Z]+)', (req, res) => {
-	const name = req.params.name.toLowerCase();
-	const pokemon = pokemones.find((p) => p.name.toLowerCase() === name);
-
-	if (!pokemon) {
-		return res.status(404).send('No se encontró el pokemon');
-	}
-
-	return res.status(200).send(pokemon);
-});
-
-module.exports = pokemonRouter;
+module.exports = pokemon;
